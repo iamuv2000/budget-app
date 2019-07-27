6ef7f14913e7acdf2ce60e822f8ea60d287168c2
iamyuvi2000 <46534175+iamyuvi2000@users.noreply.github.com>
2019-07-27 11:48:01 +0530

    Integrating Expense Summary component

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ExpenseList from'./ExpenseList'
 import ExpenseListFilters from './ExpenseListFilters'
+import ExpesnesSummary from './ExpensesSummary'
 
 const ExpenseDashboardPage=()=>(
     <div>
         <ExpenseListFilters />
+        <ExpesnesSummary />
         <ExpenseList />
     </div>
 )
--- src/selectors/expense-total.js
+++ src/selectors/expense-total.js
@@ -1,10 +1,6 @@
-import React from 'react'
-
-
-const expenseTotal=(expenses)=>{
-    return expenses
-            .map((expense)=>expense.amount)
-            .reduce((sum,value)=>sum+value,0)
+export default (expenses)=>{
+    return expenses 
+        .map((expense)=>expense.amount)
+        .reduce((sum,value)=>sum+value,0)
 }
 
-export default selectExpensetotal
