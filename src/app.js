11647800ffc59d63b02a42cb83e123e0d45a2e33
iamyuvi2000 <46534175+iamyuvi2000@users.noreply.github.com>
2019-08-10 00:01:29 +0530

    Get firebase expenses on start

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
-import {addExpense} from './actions/expenses'
+import {startSetExpenses} from './actions/expenses'
 import {setTextFilter} from './actions/filters'
 import getVisibleExpenses from'./selectors/expenses'
 import { Provider } from 'react-redux'
@@ -13,19 +13,14 @@ import './firebase/firebase'
 
 const store=configureStore()
 
-const expenseOne= store.dispatch(addExpense({description: 'Water Bill', amount:100, createdAt:100}))
-const expenseTwo= store.dispatch(addExpense({description: 'Gas Bill', amount:10000, createdAt:130}))
-const expenseThree= store.dispatch(addExpense({description: 'Rent', amount:300, createdAt:1}))
-
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses,state.filters)
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
     </Provider>
 )
 
-ReactDOM.render(jsx, document.getElementById('app'))
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
+store.dispatch(startSetExpenses()).then(()=>{
+    ReactDOM.render(jsx, document.getElementById('app'))
+})
 
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,19 +11,140 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database()
-database.ref().set({
-    name: 'Yuvraj Singh',
-    age: 19,
-    isSingle: false,
-    location:{
-      city:'Pune',
-      country:'India'
-    }
-}).then(()=>{
-  console.log('Data is saved')
-}).catch((e)=>{
-  console.log('This failed.',e)
-})
+
+export {
+  firebase,
+  database as default
+}
+
+
+
+
+
+
+
+
+
+//  //child_remove
+//  database.ref('expenses').on('child_removed',(snapshot)=>{
+//   console.log(snapshot.key, snapshot.val())
+//  })
+
+//  //child_changed
+//  database.ref('expenses').on('child_changed',(snapshot)=>{
+//   console.log(snapshot.key,snapshot.val())
+//  })
+
+// //child_added
+// database.ref('expenses').on('child_added',(snapshot)=>{
+//   console.log(snapshot.key,snapshot.val())
+// })
+
+// database.ref('expenses')
+//         .once('value')
+//         .then((snapshot)=>{
+//           const expenses = [];
+//           snapshot.forEach((childSnapshot) => {
+//             expenses.push({
+//               id: childSnapshot.key,
+//               ...childSnapshot.val()
+//             })
+//           });
+//           console.log(expenses)
+//         });
+
+// database.ref('expenses').on('value',(snapshot)=>{
+//   const expenses = [];
+//   snapshot.forEach((childSnapshot) => {
+//     expenses.push({
+//       id: childSnapshot.key,
+//       ...childSnapshot.val()
+//       })
+//   });
+//   console.log(expenses)
+// })
+
+
+// database.ref('expenses').push({
+//     description: 'Zoomcar',
+//     note: 'i20',
+//     amount:'4000',
+//     createdAt: '5-aug-2019'
+// })
+
+// database.ref('expenses').push({
+//   description: 'Airbnb',
+//   note: 'Smondo 2.0',
+//   amount:'6000',
+//   createdAt: '3-aug-2019'
+// })
+
+// database.ref('expenses').push({
+//   description: 'Ishika is bae',
+//   note: 'Ishi!',
+//   amount:'700',
+//   createdAt: '2 aug 2019'
+// })
+
+
+
+
+
+
+// database.ref(notes/-LlGqBjebYNKrTfllwuI).remove()
+// database.ref('notes').push({
+//   title: 'NodeJS',
+//   body: 'Promises!'
+// })
+
+
+// database.ref().on('value',(snapshot)=>{
+//   console.log(snapshot.val())
+// },(e)=>{
+//   console.log('Error',e)
+// })
+
+// setTimeout(() => {
+//   database.ref('age').set(20)
+// }, 3500);
+
+// setTimeout(() => {
+//   database.ref('age').set(21)
+// }, 7000);
+
+// setTimeout(() => {
+//   database.ref().off()
+// }, 10000);
+
+// setTimeout(() => {
+//   database.ref('age').set(23)
+// }, 13500);
+
+
+// database.ref("location/city")
+//   .once('value')
+//   .then((snapshot)=>{
+//     const val = snapshot.val()
+//     console.log(val)
+//   })
+//   .catch((e)=>{
+//     console.log(e)
+//   })
+
+
+// database.ref().set({
+//     name: 'Yuvraj Singh',
+//     age: 19,
+//     isSingle: false,
+//     location:{
+//       city:'Pune',
+//       country:'India'
+//     }
+// }).then(()=>{
+//   console.log('Data is saved')
+// }).catch((e)=>{
+//   console.log('This failed.',e)
+// })
 
 // database.ref().set('This is my data')
 
@@ -55,9 +176,9 @@ database.ref().set({
 // })
 
 
-database.ref().update({
-  isSingle: true,
-  name: 'Yuvi',
-  job: 'MERN Stack developer',
-  "location/city": 'Vellore'
-})
+// database.ref().update({
+//   isSingle: true,
+//   name: 'Yuvi',
+//   job: 'MERN Stack developer',
+//   "location/city": 'Vellore'
+// })
